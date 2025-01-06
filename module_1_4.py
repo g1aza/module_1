e1fd9e27a3835572673af9cc172bedc64a328d2e
g1aza <egor16811@gmail.com>
2025-01-06 14:42:38 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,5 @@
+homework_count = 12
+homework_hours = 1.5
+course_name = 'Python'
+homework_per_hour = homework_hours / homework_count
+print('Course:', course_name, 'total homework:', homework_count, 'total hours:', homework_hours, 'time per homework', homework_per_hour)
