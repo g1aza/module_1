1fb2f2371e85836548ed8376a15c9115cb55fedf
g1aza <egor16811@gmail.com>
2025-01-06 14:55:52 +0300

    Add files via upload

---
+++ module_1_7.py
@@ -0,0 +1,8 @@
+immutable_var = 4, 1.2, True, 'Test'
+print(immutable_var)
+# immutable_var[2] = 26
+# print(immutable_var)
+mutable_list = [22, 34, 11, 'green', True]
+print(mutable_list)
+mutable_list[4] = False
+print(mutable_list)
